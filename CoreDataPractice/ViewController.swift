9e9141e466d57e8a25b8a8fe10412cbecb302eb4
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-27 21:44:11 -0700

    Initial Commit

---
+++ CoreDataPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataPractice
+//
+//  Created by Prudhvi Gadiraju on 3/27/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
