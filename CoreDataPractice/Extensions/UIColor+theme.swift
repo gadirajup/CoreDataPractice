0b22f9a797d63134e97c9ff775cbd8ca51aae671
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-27 22:24:49 -0700

    - Added TableView

---
+++ CoreDataPractice/Extensions/UIColor+theme.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+theme.swift
+//  CoreDataPractice
+//
+//  Created by Prudhvi Gadiraju on 3/27/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+}
--- CoreDataPractice/ViewController.swift
+++ CoreDataPractice/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         
         setupView()
         setupNavigationBar()
+        setupTableView()
     }
     
     fileprivate func setupView() {
@@ -23,22 +24,51 @@ class ViewController: UIViewController {
     }
     
     fileprivate func setupNavigationBar() {
-        let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
-        
         navigationItem.title = "Title"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAddCompanyButtonTapped))
         
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.barTintColor = .lightRed
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.tableFooterView = UIView()
     }
     
     @objc
     func handleAddCompanyButtonTapped() {
         print("Tapped")
     }
-
-
 }
 
+// MARK:- Table View Functions
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+         return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+}
